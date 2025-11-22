a4d764e327ec959e3b0e59dc64d3ff25d79d2c6e
Simon Martinelli <simon@martinelli.ch>
2025-11-22 08:59:45 +0100

    Add detailed README and update archetype metadata
    - Introduced a comprehensive `README.md` outlining project structure, technologies, and build/testing instructions.
    - Updated `archetype-metadata.xml` to include the newly added README file.
    - Renamed main application classes to align with standard naming conventions.

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements AppShellConfigurator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VjApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
