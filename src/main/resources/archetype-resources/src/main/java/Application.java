7a0c35a6ab85b7891ccbe00c1b113b3d20037681
Simon Martinelli <simon@martinelli.ch>
2025-12-19 14:11:08 +0100

    Refactor imports in `MainLayout` and `Application` to improve readability and consistency

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,7 +1,8 @@
 package ${package};
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
